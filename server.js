3bf60465e2f5b3bb4b26e1298e1e923448bec4f7
HP <gdikshan10@gmail.com>
2023-03-14 16:49:55 +0545

    First commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({extended:true}))
+
+PORT = 3000
+let users= ['ram','shyam']
+
+app.get('/users',(req, res)=> {
+  res.send(users)
+})
+
+
+// 127.0.0.1:3000/add_user?name=sagar
+
+app.get('/add_user',(req, res)=> {
+  if(req.query.name){
+    users.push(req.query.name)
+    res.send("user added")
+  }else{
+    res.send("please provide name")
+  }
+})
+
+app.post('/add_post',(req, res)=> {
+  if(req.body.name){
+    users.push(req.body.name)
+    res.send(" added name")
+  }else{
+    res.send('please provide name')
+  }
+})
+
+app.get('/del_user',(req, res)=> {
+  if(req.query.name){
+    users=users.filter((user)=>{
+      return user !== req.query.name
+    })
+    res.send("user deleted")
+  }else{
+    res.send("please provide name")
+  }
+})
+
+app.listen(PORT,()=>{
+  console.log(`server is runnning pn port ${PORT}`)
+})
